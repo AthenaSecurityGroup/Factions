ae0c9ff8f81b2da7a0826c0ee3c881122560c488
Jonathan M. Lane <jmlane@gmail.com>
2018-07-16 04:14:10 -0300

    Added remaining Greek rifle squad units and weapons

--- config.cpp
+++ config.cpp
@@ -3,10 +3,26 @@ class CfgPatches {
         author = "Athena Security Group";
         name = "ASG Factions";
         url = "https://athenasecuritygroup.weebly.com/";
-        units[] = {"Greek_Soldier_base_F", "Greek_Soldier_F", "Greek_Soldier_TL_F"};
-        weapons[] = {};
-        requiredAddons[] = {"A3_Characters_F_INDEP"};
-        authors[] = {"DEL-J", "jmlane"};
+        units[] = {
+            "Greek_Soldier_base_F",
+            "Greek_Soldier_F",
+            "Greek_Soldier_AR_F",
+            "Greek_Soldier_LAT_F",
+            "Greek_Soldier_M_F",
+            "Greek_Soldier_SL_F",
+            "Greek_Soldier_TL_F"
+    };
+        weapons[] = {
+            "Greek_AR_ACO_F"
+};
+        requiredAddons[] = {
+            "A3_Characters_F_INDEP",
+            "A3_Weapons_F_Exp_Machineguns_LMG_03"
+        };
+        authors[] = {
+            "DEL-J",
+            "jmlane"
+        };
     };
 };
 
@@ -32,6 +48,18 @@ class CfgPatches {
     "ItemWatch", \
     "ItemRadio"
 
+class CfgWeapons {
+    class LMG_03_F;
+    class Greek_AR_ACO_F : LMG_03_F {
+        class LinkedItems {
+            class LinkedItemsOptics {
+                item = "optic_ACO_grn";
+                slot = "CowsSlot";
+            };
+        };
+    };
+};
+
 class CfgVehicles {
     class I_Soldier_base_F;
     class Greek_Soldier_base_F : I_Soldier_base_F {
@@ -53,6 +81,79 @@ class CfgVehicles {
         scope = 2;
     };
 
+    class Greek_Soldier_AR_F : Greek_Soldier_base_F {
+        class SpeechVariants
+        {
+            class Default
+            {
+                speechSingular[] = {"veh_infantry_MG_s"};
+                speechPlural[] = {"veh_infantry_MG_p"};
+            };
+        };
+        textSingular = "machinegunner";
+        textPlural = "machinegunners";
+        cost = 110000;
+        displayName = "Autorifleman";
+        icon = "iconManMG";
+        nameSound = "veh_infantry_MG_s";
+        role = "MachineGunner";
+        scope = 2;
+        threat[] = {1, 0.1, 0.3};
+        weapons[] = {"Greek_AR_ACO_F", "Throw", "Put"};
+        respawnWeapons[] = {"Greek_AR_ACO_F", "Throw", "Put"};
+        magazines[] = {
+            "200Rnd_556x45_Box_F",
+            "200Rnd_556x45_Box_F",
+            "200Rnd_556x45_Box_F",
+            "SmokeShell",
+            "SmokeShell",
+            "SmokeShell",
+            "SmokeShell"
+        };
+        respawnMagazines[] = {
+            "200Rnd_556x45_Box_F",
+            "200Rnd_556x45_Box_F",
+            "200Rnd_556x45_Box_F",
+            "SmokeShell",
+            "SmokeShell",
+            "SmokeShell",
+            "SmokeShell"
+        };
+    };
+
+    class Greek_Soldier_LAT_F : Greek_Soldier_base_F {
+        class SpeechVariants
+        {
+            class Default
+            {
+                speechSingular[] = {"veh_infantry_AT_s"};
+                speechPlural[] = {"veh_infantry_AT_p"};
+            };
+        };
+        textSingular = "AT soldier";
+        textPlural = "AT soldiers";
+        cost = 180000;
+        displayName = "Rifleman (AT)";
+        icon = "iconManAT";
+        nameSound = "veh_infantry_AT_s";
+        role = "MissileSpecialist";
+        scope = 2;
+        threat[] = {1, 0.7, 0.3};
+        weapons[] += {"launch_NLAW_F"};
+        respawnWeapons[] += {"launch_NLAW_F"};
+        secondaryAmmoCoef = 0.5;
+        backpack = "I_Fieldpack_oli_LAT";
+    };
+
+    class Greek_Soldier_M_F : Greek_Soldier_base_F {
+        cost = 110000;
+        displayName = "Marksman";
+        role = "Marksman";
+        scope = 2;
+        weapons[] = {"arifle_Mk20_MRCO_F", "Throw", "Put"};
+        respawnWeapons[] = {"arifle_Mk20_MRCO_F", "Throw", "Put"};
+    };
+
     class Greek_Soldier_TL_F : Greek_Soldier_base_F {
         cost = 450000;
         displayName = "Team Leader";
@@ -60,6 +161,35 @@ class CfgVehicles {
         scope = 2;
         magazines[] += {"HandGrenade", "HandGrenade"};
         respawnMagazines[] += {"HandGrenade", "HandGrenade"};
+    };
+
+    class Greek_Soldier_SL_F : Greek_Soldier_base_F {
+        cost = 500000;
+        displayName = "Squad Leader";
+        icon = "iconManLeader";
+        scope = 2;
+        weapons[] = {"arifle_Mk20_GL_ACO_F", "Throw", "Put"};
+        respawnWeapons[] = {"arifle_Mk20_GL_ACO_F", "Throw", "Put"};
+        magazines[] += {
+            "1Rnd_HE_Grenade_shell",
+            "1Rnd_HE_Grenade_shell",
+            "1Rnd_HE_Grenade_shell",
+            "1Rnd_HE_Grenade_shell",
+            "1Rnd_Smoke_Grenade_shell",
+            "1Rnd_Smoke_Grenade_shell",
+            "1Rnd_Smoke_Grenade_shell",
+            "1Rnd_Smoke_Grenade_shell"
+};
+        respawnMagazines[] += {
+            "1Rnd_HE_Grenade_shell",
+            "1Rnd_HE_Grenade_shell",
+            "1Rnd_HE_Grenade_shell",
+            "1Rnd_HE_Grenade_shell",
+            "1Rnd_Smoke_Grenade_shell",
+            "1Rnd_Smoke_Grenade_shell",
+            "1Rnd_Smoke_Grenade_shell",
+            "1Rnd_Smoke_Grenade_shell"
+        };
     };
 };
 
